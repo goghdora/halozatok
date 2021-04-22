9f720bea627150cc647112d898c22d8c9a634fb6
Dóra Gőgh <g.dori2012@gmail.com>
2021-04-22 18:11:24 +0200

    Pascal 3szog

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE HTML>
+<HTML lang="hu">
+<head>
+    <meta charset="utf-8">
+    <title>Hajós teszt</title>
+</head>
+<body>
+     //<script>
+                var kérdések;
+
+                function letöltésBefejeződött(d) {
+                    console.log("Sikeres letöltés")
+                    console.log(d)
+                    kérdések=d;
+                }
+
+                fetch(`/questions.json`)
+                    .then(response => response.json())
+                    .then(data => letöltésBefejeződött(data)
+                );
+
+                window.onload(letöltésBefejeződött());
+     </script>
+</body> 
+</HTML> 
---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,39 @@
+﻿function factorial(n) {
+    let result = 1;
+    for (var i = 1; i <= n; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
+function pascal(n, k) {
+    let result = factorial(n) / (factorial(k) * factorial(n - k))
+    return result;
+}
+
+window.onload = function () {
+    let containerElement = document.getElementById("container");
+    for (var i = 0; i < 10; i++) {
+        let elementToInsert = document.createElement("div");
+        elementToInsert.style.backgroundColor = `rgba(0, 0, 0, 0.${i})`;
+        elementToInsert.innerHTML = `${i + 1}`;
+        containerElement.appendChild(elementToInsert);
+    }
+
+    let pascalElement = document.getElementById("pascal");
+    for (var i = 0; i < 10; i++) {
+        let elementToInsert = document.createElement("div");
+        elementToInsert.setAttribute("id", `row_${i + 1}`);
+        pascalElement.appendChild(elementToInsert);
+        let rowElement = document.getElementById(`row_${i + 1}`);
+
+        for (var j = 0; j < i + 1; j++) {
+            let elementToInsert = document.createElement("div");
+            elementToInsert.setAttribute("id", `child_${j + 1}`);
+            elementToInsert.innerHTML = `${pascal(i, j)}`;
+            rowElement.appendChild(elementToInsert);
+        }
+
+    }
+
+}
